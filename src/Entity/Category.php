4695b76bfb2664d13df6c25b09ae224c65680de5
RAJOELISON Aina Tiavina <rajoelisonainatiavina@gmail.com>
2022-01-16 13:11:45 +0300

    We display only the essential field

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -7,9 +7,12 @@ use App\Repository\CategoryRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: CategoryRepository::class)]
-#[ApiResource]
+#[ApiResource(
+    normalizationContext: ['groups' => 'read:collections'],
+)]
 class Category
 {
     #[ORM\Id]
@@ -18,6 +21,7 @@ class Category
     private $id;
 
     #[ORM\Column(type: 'string', length: 255)]
+    #[Groups(['read:collections'])]
     private $name;
 
     #[ORM\OneToMany(mappedBy: 'Category', targetEntity: Post::class)]
--- src/Entity/Post.php
+++ src/Entity/Post.php
@@ -5,19 +5,22 @@ namespace App\Entity;
 use ApiPlatform\Core\Annotation\ApiResource;
 use App\Repository\PostRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: PostRepository::class)]
 #[ApiResource(
-    normalizationContext: ['group' => 'read:collections']
+    normalizationContext: ['groups' => 'read:collections']
 )]
 class Post
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column(type: 'integer')]
+    #[Groups(['read:collections'])]
     private $id;
 
     #[ORM\Column(type: 'string', length: 255)]
+    #[Groups(['read:collections'])]
     private $title;
 
     #[ORM\Column(type: 'text')]
