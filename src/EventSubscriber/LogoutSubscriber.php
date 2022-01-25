6f394ba6b8cc5f00403a2a4b0a2670a4aa8091e4
RAJOELISON Aina Tiavina <rajoelisonainatiavina@gmail.com>
2022-01-25 17:31:47 +0300

    Add and configure logout path

---
+++ src/EventSubscriber/LogoutSubscriber.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use ApiPlatform\Core\Bridge\Symfony\Bundle\Test\Response;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response as HttpFoundationResponse;
+use Symfony\Component\Security\Http\Event\LogoutEvent;
+
+class LogoutSubscriber implements EventSubscriberInterface
+{
+    public function onLogoutEvent(LogoutEvent $event)
+    {
+        if(in_array('application/json', $event->getRequest()->getAcceptableContentTypes())){
+            return $event->setResponse(new JsonResponse(null, 
+            HttpFoundationResponse::HTTP_NO_CONTENT));
+        }
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            LogoutEvent::class => 'onLogoutEvent',
+        ];
+    }
+}
